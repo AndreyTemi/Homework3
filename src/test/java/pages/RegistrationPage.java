9c24248038dbc7f9b4b6e2bfc243ec790cf69413
Andrey Temirbaev <temir@starlink.ru>
2023-03-14 01:17:38 +0300

    HomeworkPages - Faker

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -2,7 +2,7 @@ package pages;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import pages.components.CalendarComponent;
-import pages.components.VerifyResultComponent;
+
 
 import java.io.File;
 
