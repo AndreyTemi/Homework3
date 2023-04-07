71619f9e98021f7cc8c493eefe8071de8e4f88b0
Andrey <89055298910a@gmail.com>
2023-04-08 02:59:56 +0300

    forJenkins2 - param

--- src/test/java/tests/TestBaseRemote.java
+++ src/test/java/tests/TestBaseRemote.java
@@ -16,10 +16,10 @@ public class TestBaseRemote {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browser_size","1920x1080");
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("baseUrlSelenoid","selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
